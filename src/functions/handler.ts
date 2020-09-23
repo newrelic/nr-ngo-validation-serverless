5a6ec872c282cf2d3b3fcb209f71c8630b040ee4
norberdo <norbert.suski@gmail.com>
2020-09-23 10:07:26 +0200

    feat: lint-staged added to project

--- src/functions/handler.ts
+++ src/functions/handler.ts
@@ -1,13 +1,20 @@
 import { APIGatewayProxyHandler, APIGatewayEvent, Context } from 'aws-lambda';
 
-
-export const handle: APIGatewayProxyHandler = async (event: APIGatewayEvent, _context: Context) => {
+export const handle: APIGatewayProxyHandler = async (
+  event: APIGatewayEvent,
+  _context: Context
+) => {
+  // eslint-disable-next-line no-console
+  console.log(_context);
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Your function executed successfully!',
         input: event,
-      }, null, 2),
+      },
+      null,
+      2
+    ),
   };
 };
