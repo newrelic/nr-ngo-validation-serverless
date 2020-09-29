966badf7799f722e590342e9055783229ce9c3d3
norberdo <norbert.suski@gmail.com>
2020-09-29 09:19:58 +0200

    feat: tests for http util added

--- tests/utils/http-util.spec.ts
+++ tests/utils/http-util.spec.ts
@@ -1,11 +1,26 @@
-import { createLookupApiUrl } from '../../src/utils/http-util';
+import {
+  createConstraintApiUrl,
+  createLookupApiUrl,
+} from '../../src/utils/http-util';
+import { config } from '../../src/config';
 
 describe('HTTP util tests', () => {
+  const sessionKey = config.SESSION_KEY;
+
   it('Create valid lookup api url', () => {
-    const apiUrl = 'https://myapi.com/v1';
-    const token = 'token';
-    const expectedUrl = 'https://myapi.com/v1/sessionKey/?token=token';
+    const apiUrl = config.LOOKUP_API_URL;
+    const token = 'pin@handle';
+    const expectedUrl = `${apiUrl}/${sessionKey}/?token=${token}`;
 
     expect(createLookupApiUrl(apiUrl, token)).toEqual(expectedUrl);
+  });
+
+  it('Create valid constraint api url', () => {
+    const apiUrl = config.CONSTRAINT_API_URL;
+    const constraintId = config.CONSTRAINT_ID;
+    const orgId = 'RandomOrgId';
+    const expectedUrl = `${apiUrl}/${sessionKey}/?constraint_id=${constraintId}&org_id=${orgId}`;
+
+    expect(createConstraintApiUrl(apiUrl, orgId)).toEqual(expectedUrl);
   });
 });
