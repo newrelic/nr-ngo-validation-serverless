5982eabf131428af92c67269fcbc63fe97f05d15
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-09-30 10:41:15 +0200

    test: added fixtures and token validator scenario

---
+++ tests/utils/token-validator.spec.ts
@@ -0,0 +1,9 @@
+describe('Lookup API validation path', () => {
+  it('Provided token has got proper format', () => {});
+});
+
+describe('Lookup response validation', () => {
+  it('Correct response from lookup api should pass the validation check', () => {});
+
+  it('Incorrect response (empty array) from lookup api should return no data for provided token', () => {});
+});
