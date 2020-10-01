ab4075d369db31ec6588ffe32e6cfa0525bb9ee2
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-10-01 14:56:22 +0200

    test: prepared object responses for constraint api

---
+++ tests/fixtures/constraint-api-fixtures.ts
@@ -0,0 +1,8 @@
+import { ConstraintResponse } from '../../src/types/constraintResponse';
+
+export const ConstraintApiResponse = {
+  allPassResponse: {},
+  tokenExpiredResponse: {},
+  organizationStatusFailed: {},
+  eligibilityStatusFalse: {},
+};
