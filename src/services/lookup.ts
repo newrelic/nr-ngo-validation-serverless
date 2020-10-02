f7db8631d34a12ae4466f69202cafa76428c892c
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-10-02 11:26:06 +0200

    refactor: separated lookup and constraint from lambda code

---
+++ src/services/lookup.ts
@@ -0,0 +1,22 @@
+import { config } from '../config';
+import { LookupLargeResponse } from '../types/lookupLargeResponse';
+import { sendGetRequest, createLookupApiUrl } from '../utils/http-util';
+import { LambdaResponses } from '../utils/lambda-responses';
+import { validateLookupResponse } from '../utils/token-validator';
+
+export const getResponseFromLookup = async (
+  token: string,
+): Promise<LookupLargeResponse | LambdaResponses> => {
+  const lookUpApiUrl = createLookupApiUrl(config.LOOKUP_API_URL, token);
+  const lookupRes = await sendGetRequest<LookupLargeResponse>(lookUpApiUrl);
+
+  // TODO: Validate token expiration date
+
+  const validatedLookupResponse = validateLookupResponse(lookupRes);
+
+  if (validatedLookupResponse === LambdaResponses.noDataForProvidedToken) {
+    return validatedLookupResponse;
+  }
+
+  return validatedLookupResponse;
+};
