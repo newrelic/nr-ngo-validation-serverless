ae36f27c6d959b47f5d6cf0cf42c5ffededeff63
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-10-13 18:43:41 +0200

    refactor: removed unused method

--- src/services/constraint.ts
+++ src/services/constraint.ts
@@ -1,9 +1,7 @@
 import { config } from '../config';
 import { ConstraintResponse } from '../types/constraintResponse';
 import { LookupLargeResponse } from '../types/lookupLargeResponse';
-import { LambdaResponse } from '../types/response';
 import { createConstraintApiUrl, sendGetRequest } from '../utils/http-util';
-import { LambdaResponses } from '../utils/lambda-responses';
 
 export const getResponseFromConstraint = async (orgId: string): Promise<ConstraintResponse> => {
   const constraintApiUrl = createConstraintApiUrl(config.CONSTRAINT_API_URL, orgId);
@@ -15,12 +13,4 @@ export const getResponseFromConstraint = async (orgId: string): Promise<Constrai
 
 export const getOrgId = (lookupResponse: LookupLargeResponse): string => {
   return lookupResponse.returnStatus.data[0].org_id;
-};
-
-export const checkEligibility = (response: ConstraintResponse): LambdaResponse => {
-  if (response.returnStatus.data[0].eligibility_status) {
-    return LambdaResponses.eligible;
-  }
-
-  return LambdaResponses.notEligible;
 };
