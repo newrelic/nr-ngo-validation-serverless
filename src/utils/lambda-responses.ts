ff165a73d1c957cade241118b61b3b99f3367ed6
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-10-13 15:06:57 +0200

    feat: adjusted lambda responses for frontend contract

--- src/utils/lambda-responses.ts
+++ src/utils/lambda-responses.ts
@@ -3,41 +3,31 @@ import { LambdaResponse } from '../types/response';
 export class LambdaResponses {
   public static readonly noTokenProvided: LambdaResponse = {
     statusCode: 400,
-    body: JSON.stringify({ message: 'No token provided' }, null, 2),
+    body: JSON.stringify({ internalStatusCode: 40001, message: 'No token provided' }),
   };
 
   public static readonly badTokenProvided: LambdaResponse = {
     statusCode: 400,
-    body: JSON.stringify({ message: 'Bad token provided' }, null, 2),
-  };
-
-  public static readonly noDataForProvidedToken: LambdaResponse = {
-    statusCode: 404,
-    body: JSON.stringify({ message: 'No data for provided token' }, null, 2),
-  };
-
-  public static readonly noEndpoint: LambdaResponse = {
-    statusCode: 404,
-    body: JSON.stringify({ message: 'This endpoint does not exist' }, null, 2),
+    body: JSON.stringify({ internalStatusCode: 40002, message: 'Bad token provided' }),
   };
 
   public static readonly tokenExpired: LambdaResponse = {
     statusCode: 401,
-    body: JSON.stringify({ message: 'Verified - TechSoup Token expired' }, null, 2),
+    body: JSON.stringify({ internalStatusCode: 40101, message: 'Verified - TechSoup Token expired' }),
   };
 
   public static readonly notQualified: LambdaResponse = {
     statusCode: 401,
-    body: JSON.stringify({ message: 'Sorry you do not qualified' }, null, 2),
+    body: JSON.stringify({ internalStatusCode: 40102, message: 'Sorry you do not qualified' }),
   };
 
-  public static readonly notEligible: LambdaResponse = {
-    statusCode: 401,
-    body: JSON.stringify({ message: 'Sorry you are not eligible' }, null, 2),
+  public static readonly noEndpoint: LambdaResponse = {
+    statusCode: 404,
+    body: JSON.stringify({ internalStatusCode: 40401, message: 'This endpoint does not exist' }),
   };
 
-  public static readonly eligible: LambdaResponse = {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Success, you qualify!' }, null, 2),
+  public static readonly noDataForProvidedToken: LambdaResponse = {
+    statusCode: 404,
+    body: JSON.stringify({ internalStatusCode: 40402, message: 'No data for provided token' }),
   };
 }
