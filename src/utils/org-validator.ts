127cc3967d5799e0d06a61d75a3c58c3fc0fdde7
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-10-13 15:09:27 +0200

    refactor: set up the validate organisation method

--- src/utils/org-validator.ts
+++ src/utils/org-validator.ts
@@ -1,3 +1,4 @@
-export const validateOrg = (org: string): Error => {
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export const validateOrg = (_org: string): Error => {
   throw new Error('You need to implement this method by yourself for your validation process');
 };
