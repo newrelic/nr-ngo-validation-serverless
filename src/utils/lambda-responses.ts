428308d374f2ac892ab6f4e6219e3914114465b3
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-10-15 10:35:34 +0200

    refactor: used status codes from library

--- src/utils/lambda-responses.ts
+++ src/utils/lambda-responses.ts
@@ -1,34 +1,34 @@
 import { LambdaResponse } from '../types/response';
-import { Status } from '../utils/status';
+import { StatusCodes } from 'http-status-codes';
 
 export class LambdaResponses {
   public static readonly noTokenProvided: LambdaResponse = {
-    statusCode: Status.HttpStatus.BadRequest,
+    statusCode: StatusCodes.BAD_REQUEST,
     body: JSON.stringify({ internalStatusCode: 40001, message: 'No token provided' }),
   };
 
   public static readonly badTokenProvided: LambdaResponse = {
-    statusCode: Status.HttpStatus.BadRequest,
+    statusCode: StatusCodes.BAD_REQUEST,
     body: JSON.stringify({ internalStatusCode: 40002, message: 'Bad token provided' }),
   };
 
   public static readonly tokenExpired: LambdaResponse = {
-    statusCode: Status.HttpStatus.Unauthorized,
+    statusCode: StatusCodes.UNAUTHORIZED,
     body: JSON.stringify({ internalStatusCode: 40101, message: 'Verified - TechSoup Token expired' }),
   };
 
   public static readonly notQualified: LambdaResponse = {
-    statusCode: Status.HttpStatus.Unauthorized,
+    statusCode: StatusCodes.UNAUTHORIZED,
     body: JSON.stringify({ internalStatusCode: 40102, message: 'Sorry you do not qualified' }),
   };
 
   public static readonly noEndpoint: LambdaResponse = {
-    statusCode: Status.HttpStatus.NotFound,
+    statusCode: StatusCodes.NOT_FOUND,
     body: JSON.stringify({ internalStatusCode: 40401, message: 'This endpoint does not exist' }),
   };
 
   public static readonly noDataForProvidedToken: LambdaResponse = {
-    statusCode: Status.HttpStatus.NotFound,
+    statusCode: StatusCodes.NOT_FOUND,
     body: JSON.stringify({ internalStatusCode: 40402, message: 'No data for provided token' }),
   };
 }
