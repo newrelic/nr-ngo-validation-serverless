c7d3525d178cf2b9eaefdd2195d7734a87a30d5d
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-11-12 11:04:27 +0100

    feat: extended url creation

--- src/utils/http-util.ts
+++ src/utils/http-util.ts
@@ -13,10 +13,38 @@ export const sendGetRequest = async <T>(url: string): Promise<T> => {
   }
 };
 
-export const createLookupApiUrl = (apiUrl: string, token: string): string => {
+/**
+ * Creates request URL for Lookup API.
+ *
+ * @param apiUrl URL to API
+ * @param token Lookup token
+ * @param sessionKey Your session key; also known as API Key.
+ */
+export const createLookupApiUrl = (apiUrl: string, token: string, sessionKey?: string): string => {
+  if (sessionKey) {
+    return `${apiUrl}/${sessionKey}/?token=${token}`;
+  }
+
   return `${apiUrl}/${config.SESSION_KEY}/?token=${token}`;
 };
 
-export const createConstraintApiUrl = (apiUrl: string, orgId: string): string => {
+/**
+ * Creates request URL for Constraing API.
+ *
+ * @param apiUrl URL to API
+ * @param orgId Id of your organization.
+ * @param sessionKey Your session key; also known as API Key.
+ * @param constraintId
+ */
+export const createConstraintApiUrl = (
+  apiUrl: string,
+  orgId: string,
+  sessionKey?: string,
+  constraintId?: string,
+): string => {
+  if (sessionKey && constraintId) {
+    return `${apiUrl}/${sessionKey}/?constraint_id=${constraintId}&org_id=${orgId}`;
+  }
+
   return `${apiUrl}/${config.SESSION_KEY}/?constraint_id=${config.CONSTRAINT_ID}&org_id=${orgId}`;
 };
