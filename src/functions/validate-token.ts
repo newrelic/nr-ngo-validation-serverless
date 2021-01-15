3618093d67c4eccdeec8c842ce630fab119d9429
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-01-15 09:58:39 +0100

    feat: added basic query for dynamodb to test the db settings

--- src/functions/validate-token.ts
+++ src/functions/validate-token.ts
@@ -1,12 +1,32 @@
 import { APIGatewayEvent } from 'aws-lambda';
 import { LambdaResponse } from '../types/response';
+import { ValidationAttemptsModel } from '../model/validation-attempts';
+import { isTokenValid } from '../utils/token-validator';
+import { LambdaResponses } from '../utils/lambda-responses';
 
+/**
+ * Checks if the provided token exists in the database.
+ *
+ * @param event Incoming event from API Gateway
+ */
 export const validateToken = async (event: APIGatewayEvent): Promise<LambdaResponse> => {
+  const params = event.queryStringParameters || {};
+
+  if (!isTokenValid(params.token)) {
+    return LambdaResponses.badTokenProvided;
+  }
+
+  if (!params.account_id) {
+    return LambdaResponses.missingRequiredData;
+  }
+
+  const queryItem = await ValidationAttemptsModel.query('account_id').eq(params.account_id).exec();
+
   return {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
     statusCode: 200,
-    body: JSON.stringify(''),
+    body: JSON.stringify(queryItem),
   };
 };
