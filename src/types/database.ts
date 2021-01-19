27852c21f0aeadf44249e691833eff1c709f1fe6
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-01-19 16:20:38 +0100

    feat: added database record type

--- src/types/database.ts
+++ src/types/database.ts
@@ -3,3 +3,14 @@ export type DatabaseContext = {
   secretArn: string;
   database: string;
 };
+
+export type ValidationAttemptsRecord = {
+  id: number;
+  account_id: number;
+  validation_date: Date;
+  org_id: string;
+  org_name: string;
+  eligibility_status: string;
+  reason: string;
+  token: string;
+};
