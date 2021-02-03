8e7b0866f02fb705b3011721f3d2cf6fdd517e62
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-02-03 14:23:15 +0100

    feat: created validation history

--- src/functions/get-validation-history.ts
+++ src/functions/get-validation-history.ts
@@ -1,56 +1,56 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
-import { Context } from 'aws-lambda/handler';
-import { ValidationAttempts } from '../types/database';
+import {
+  ValidationHistoryRequest,
+  ValidationHistoryResponse,
+  ValidationCount,
+  ValidationAttempts,
+} from '../types/database';
 import { LambdaResponse } from '../types/response';
 import { LambdaResponses } from '../utils/lambda-responses';
-import { Logger } from '../utils/logger';
-import { getValidationAttempts, getValidationAttemptsBySearchPhrase } from '../utils/database';
+import { getValidationAttempts } from '../utils/database';
+import { createSql } from '../utils/sql';
 
-export const getValidationHistory = async (event: APIGatewayProxyEvent, context: Context): Promise<LambdaResponse> => {
-  const logger = new Logger(context);
+export const getValidationHistory = async (event: APIGatewayProxyEvent): Promise<LambdaResponse> => {
   const params = event.queryStringParameters || {};
-  let accountId: string;
-  let searchPhrase: string;
-  let orderBy: string;
-  let orderAsc: boolean;
-  let limit: number;
-  let offset: number;
-  let validationHistory: ValidationAttempts;
 
-  if (params.searchPhrase && params.accountId) {
+  if (params.accountId && params.searchPhrase) {
     return LambdaResponses.badRequest;
   }
 
-  if (params.accountId) {
-    accountId = params.accountId;
-  }
-
-  if (params.searchPhrase) {
-    searchPhrase = params.searchPhrase;
-  }
-
-  if (params.orderBy && params.orderAsc && params.limit && params.offset) {
-    orderBy = params.orderBy;
-    orderAsc = !!params.orderAsc;
-    limit = Number(params.limit);
-    offset = Number(params.offset);
-  } else {
+  if (!params.startDate && !params.endDate) {
     return LambdaResponses.badRequest;
   }
 
-  if (params.accountId) {
-    logger.info('Getting validation history with account id');
-    validationHistory = await getValidationAttempts(accountId, orderBy, orderAsc, limit, offset);
-  } else {
-    logger.info('There is no account id, getting all');
-    validationHistory = await getValidationAttemptsBySearchPhrase(searchPhrase, orderBy, orderAsc, limit, offset);
-  }
+  const validationHistoryRequest: ValidationHistoryRequest = {
+    accountId: params.accountId ?? undefined,
+    searchPhrase: params.searchPhrase ?? undefined,
+    orderBy: params.orderBy ?? undefined,
+    orderAsc: !!params.orderAsc ?? undefined,
+    limit: Number(params.limit) ?? undefined,
+    offset: Number(params.offset) ?? undefined,
+    startDate: new Date(params.startDate),
+    endDate: new Date(params.endDate),
+  };
+
+  const sqlQuery = createSql(validationHistoryRequest, false);
+  const countQuery = createSql(validationHistoryRequest, true);
+
+  const validationHistory = (await getValidationAttempts(
+    sqlQuery as string,
+    validationHistoryRequest,
+  )) as ValidationAttempts;
+  const recordCount = (await getValidationAttempts(countQuery as string, validationHistoryRequest)) as ValidationCount;
+
+  const response: ValidationHistoryResponse = {
+    attempts: validationHistory.records,
+    records: recordCount.records[0].count ?? 0,
+  };
 
   return {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
     statusCode: 200,
-    body: JSON.stringify(validationHistory),
+    body: JSON.stringify(response),
   };
 };
