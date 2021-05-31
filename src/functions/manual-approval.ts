2edced9cdd605513d0052bbc3f7df891b3c27f0c
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-05-31 09:26:18 +0200

    feat: added validation source enum

---
+++ src/functions/manual-approval.ts
@@ -0,0 +1,19 @@
+import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import { LambdaResponse } from '../types/response';
+import { LambdaResponses } from '../utils/lambda-responses';
+import { Logger } from '../utils/logger';
+
+export const manualApprove = async (event: APIGatewayProxyEvent, context: Context): Promise<LambdaResponse> => {
+  const logger = new Logger(context);
+  logger.info('Manual approval lambda...');
+
+  // accountId, validationSource, description
+
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: 200,
+    body: JSON.stringify(''),
+  };
+};
