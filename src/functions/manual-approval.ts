f39878436bc66fb0f67bea411497a6012d626161
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-06-02 11:22:52 +0200

    feat: check if accout was already manual approved

--- src/functions/manual-approval.ts
+++ src/functions/manual-approval.ts
@@ -1,7 +1,8 @@
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import { ValidationAttempts } from '../types/database';
 import { ManualApproval } from '../types/requests/manual-approval';
 import { LambdaResponse } from '../types/response';
-import { saveManualApproval } from '../utils/database';
+import { getValidationAttemptByAccountId, saveManualApproval } from '../utils/database';
 import { LambdaResponses } from '../utils/lambda-responses';
 import { Logger } from '../utils/logger';
 
@@ -13,6 +14,13 @@ export const manualApprove = async (event: APIGatewayProxyEvent, context: Contex
     const body = JSON.parse(event.body);
     const manual = body as ManualApproval;
     logger.info(JSON.stringify(manual));
+
+    logger.info('Checking if accoount is existing already...');
+    const result: ValidationAttempts = await getValidationAttemptByAccountId(manual.accountId);
+
+    if (result.records.length > 0) {
+      return LambdaResponses.accountAlreadyExist;
+    }
 
     logger.info('Saving user data for manual validation...');
     await saveManualApproval(manual.accountId, manual.description);
